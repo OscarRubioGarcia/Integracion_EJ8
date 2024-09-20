3d09fcfbc755aa9b05d1eccf90856d7c3a236dd4
Rubio García <osrubio@minsait.com>
2024-09-20 12:03:42 +0200

    Included proyect

---
+++ src/main/java/osrubio/ej8/Ej8Application.java
@@ -0,0 +1,13 @@
+package osrubio.ej8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ej8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ej8Application.class, args);
+	}
+
+}
